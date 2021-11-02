f5bddc89ba4a18e2848743226ae6fa30359c3d70
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-02 10:47:45 -0500

    added some pseudo code to understand better

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  //a function that handles click
+  const handleClick = () => {
+    //storing the getRandomColor function to the variable newCOlor
+    const newColor = getRandomColor()
+    // change the color of the background the same thing as the color argument that was passed by
+    onChangeColor(newColor)
+}
+
+  return (
+    <div
+      //have a div with a class name child
+      className="child"
+// made the color of the background the same as the color that was passed in as an argument
+      style={{ backgroundColor: color }}
+      // handles the click so that when the child was click it will change color randomilt
+      onClick={handleClick}
+    />
+  )
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,30 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  //storing the helper funciton in randomCOlor variable
   const randomColor = getRandomColor();
+  //setting color to the random color that will give us random color
   const [color, setColor] = useState(randomColor);
+//setting a variable for the children color and making it #FFF as default
+  const [childrenColor, setChildrenColor] = useState("#FFF")
+
+  //function that takes in the default value of the children color
+  const handleColorChange = (newChildrenColor) => {
+    //getting another random color
+    const newRandomColor = getRandomColor()
+    //make the color the same as the getRandomColor that was called
+    setColor(newRandomColor)
+
+    //setting the color of the children to the argument that was passed
+    setChildrenColor(newChildrenColor)
+  }
 
   return (
+    //returns a div with a className parent and setting the value to the sameColor to what we got when we call the getRandomColor function
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* calling the children and making their color the same and picking a random color */}
+      <Child color={childrenColor} onChangeColor={handleColorChange} />
+      <Child color={childrenColor} onChangeColor={handleColorChange} />
     </div>
   );
 }
